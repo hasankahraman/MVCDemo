5fc7116b8e2bea677123ab1e1d3ec8272d5a6c8d
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-09-15 22:22:58 +0300

    Category Controllers added but not finished

---
+++ BussinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BussinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> genericRepository = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return genericRepository.Get();
+        }
+
+        public void AddCategory(Category category)
+        {
+                genericRepository.Insert(category);
+        }
+
+
+
+
+    }
+}
---
+++ MVCDemo/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BussinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategories()
+        {
+            var categories = cm.GetAll();
+
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            cm.AddCategory(category);
+            return RedirectToAction("GetCategories");
+        }
+    }
+}
